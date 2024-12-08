48256427d0f0b109c6dbb2cc5e033c258be6ec43
Honey Rose Roxas <honeyrose.roxas@infor.com>
2024-12-08 15:41:32 +0800

    basic react component

---
+++ src/components/BioDetails.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export const BioDetails = () => {
+    return (<div>
+        Insert Dev Profile
+    </div>);
+}
---
+++ src/pages/Portfolio.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { BioDetails } from '../components/BioDetails';
+
+export const PortfolioPage = () => {
+    return(
+        <div>
+            <BioDetails />
+        </div>
+    );
+}
